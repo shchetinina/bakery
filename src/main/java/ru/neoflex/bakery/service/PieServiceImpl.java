8d95fa9b68646a9d4c80685a74f1f72c2ff20496
Щетинина В.Д <schetinina@dev.vtb.ru>
2022-08-29 14:44:21 +0400

    Handling case with Recipe == null in PieServiceImpl

--- src/main/java/ru/neoflex/bakery/service/PieServiceImpl.java
+++ src/main/java/ru/neoflex/bakery/service/PieServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Service;
 import ru.neoflex.bakery.dto.PieDtoView;
 import ru.neoflex.bakery.mapper.PieRecipeMapper;
 import ru.neoflex.bakery.model.Pie;
+import ru.neoflex.bakery.model.Recipe;
 import ru.neoflex.bakery.repository.PieRepository;
 import ru.neoflex.bakery.repository.RecipeRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,14 +23,19 @@ public class PieServiceImpl implements PieService {
     private final RecipeRepository recipeRepository;
 
     @Override
-    public List<PieDtoView> findAll() {
+    public List<PieDtoView> findAll() throws EntityNotFoundException {
         List<Pie> pies = pieRepository.findAll();
         List<PieDtoView> pieDtoViews = new ArrayList<>();
         PieRecipeMapper pieDtoMapper = Mappers.getMapper(PieRecipeMapper.class);
 
         for(Pie item:pies){
-            //TODO обработать случай, когда нет записи Recipe для Pie.id
-            pieDtoViews.add(pieDtoMapper.pieDtoView(item, recipeRepository.getOne(item.getId())));
+            try{
+                Recipe recipe = recipeRepository.getOne(item.getId());
+                pieDtoViews.add(pieDtoMapper.pieDtoView(item, recipe));
+            }
+            catch(EntityNotFoundException ex){
+                pieDtoViews.add(pieDtoMapper.pieDtoView(item, null));
+            }
         }
         return pieDtoViews;
     }
